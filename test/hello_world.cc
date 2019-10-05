a1ced26cb60d8039a2d705e0e8afa37f756c79d4
Alisahhh <923431911@qq.com>
2019-10-05 20:52:08 +0800

    add test

--- test/hello_world.cc
+++ test/hello_world.cc
@@ -13,38 +13,16 @@ int main(int argc, char** argv)
     printf("socket: %x\n", socket);
     if (local_rank == 0) {
         const char *msg = "Hello";
-        AMessage *amsg = AMessage_create((void *)msg, 6, 0);
-        if (send_(socket, amsg)) {
-            printf("Error to send");
-        }
-        else {
-            printf("nodeid: %d\n", amsg->node_id);
-        }
-        Socket *newSocket = RDMA_Reconnect(other_rank);
-        if (send_(newSocket, amsg)) {
-            printf("Error to send with new socket");
-        }
-        else {
-            printf("nodeid: %d\n", amsg->node_id);
-        }
-        AMessage_destroy(amsg);
-    } else {
-        AMessage *recv;
-        while(true) {
-            recv = recv_(socket);
-            if (recv == nullptr) {
-                printf("Stop\n");
-                break;
-            }
-            printf("%s, nodeId: %d\n", recv->buffer, recv->node_id);
-            socket = RDMA_Reconnect(other_rank);
-            AMessage_destroy(recv);
-        }
+        RDMA_Send(msg, 6, other_rank, 0);
     }
-
-    printf("Reach end\n");
-    if (local_rank == 0) {
-        while(1);
+    else
+    {
+        auto buf = (char *)malloc(10);
+        RDMA_Recv(buf, 6, other_rank, 0);
+        printf("local_rank = %d\n", local_rank);
+        printf("%s\n", buf);
     }
-    RDMA_Finalize();
+    while(1)
+        ;
+    // RDMA_Finalize();
 }
