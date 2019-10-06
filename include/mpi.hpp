7a7f2200100af232dd1997dd3286b8ebf80af95a
milkybird98 <milkybird98@outlook.com>
2019-10-06 21:00:58 +0800

    > fix merge

--- include/mpi.hpp
+++ include/mpi.hpp
@@ -2,23 +2,6 @@
 #define MPI_HPP
 #include "malloc.h"
 #include "fmpi.hpp"
-<<<<<<< HEAD
-    int RDMA_Send(void *buf, int count, int dest,int rdma_group);
-    int RDMA_Recv(void *buf, int count, int source, int rdma_group);
-    // datatype  0 double 1 int 2 long int
-    // op 0 sum 1 min 2 max
-    int RDMA_Reduce(void *sendbuf, void *recvbuf, int count, int datatype, int op, int root, int rdma_group);
-    int RDMA_Allreduce(void *sendbuf, void *recvbuf, int count, int datatype, int op, int rdma_group);
-    int RDMA_MakeAll(void *buf, int count_in_byte, int root, int rdma_group);
-    int RDMA_GetOffsetRank(int offset, int is_right_side, int rdma_group);
-    int RDMA_ExchangeAll_exp(void *sendbuf, int sendcount, void *recvbuf, int recvcount, int rdma_group);
-    int RDMA_ExchangeAll(void *sendbuf, int sendcount, void *recvbuf, int recvcount, int rdma_group);
-    int RDMA_GetAll(void *sendbuf, int sendcount, void *recvbuf, int recvcount, int root, int rdma_group);
-    int RDMA_Scatter(void *sendbuf, int sendcount, void *recvbuf, int recvcount, int root, int rdma_group);
-    int RDMA_Barrier();
-    int TestIrecv();
-#endif
-=======
 
 #define R_TYPE_DOUBLE 0
 #define R_TYPE_INT 1
@@ -46,4 +29,3 @@ int RDMA_Allgather(void *sendbuf, int sendcount, int sendtype, void *recvbuf, in
 int RDMA_Gather(void *sendbuf, int sendcount, int sendtype, void *recvbuf, int recvcount, int recvtype, int root);
 int RDMA_Scatter(void *sendbuf, int sendcount, int sendtype, void *recvbuf, int recvcount, int recvtype, int root);
 #endif
->>>>>>> origin/fix
